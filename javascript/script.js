8113042549ed4422832ffed1e0db9c797b9655cd
Hamsa Elmi <73555504+hamsame@users.noreply.github.com>
2021-04-15 14:22:32 +0100

    init commit
    init commit

---
+++ javascript/script.js
@@ -0,0 +1,28 @@
+let a = 0;
+const images = [];
+
+images[0] = '../images/developers.jpeg';
+images[1] = '../images/coder.jpeg';
+images[2] = '../images/coder2.jpeg';
+images[3] = '../images/coder3.jpeg';
+
+// change img
+function changeImages() {
+  document.slider.src = images[a];
+
+  a < images.length - 1 ? a++ : (a = 0);
+
+  setTimeout('changeImages()', 3000);
+}
+window.onload = changeImages();
+
+// Materialize css
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.dropdown-trigger');
+  var instances = M.Dropdown.init(elems, { hover: true });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems);
+});
